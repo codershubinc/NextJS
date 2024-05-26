f6e935fc765f57b8bd202fca9ee315eaac617333
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-05-26 14:03:26 +0530

    bfsx

--- musicwebnextjs/src/app/users/signup/page.tsx
+++ musicwebnextjs/src/app/users/signup/page.tsx
@@ -1,33 +1,15 @@
 'use client'
 import React from 'react'
 import PageUi from '@/components/page/pageui'
-import { useAuth } from '@/context/AuthContext'
+// import { useAuth } from '@/context/AuthContext'
 import SignUpForm from './signUpForm'
-import Link from 'next/link'
+// import Link from 'next/link'
 
 
 function page() {
-    const { isUserLogin } = useAuth();
+    // const { isUserLogin } = useAuth();
 
-    if (isUserLogin) {
-        return (<PageUi
-            className=' '
-        >
-            <h1
-                className='text-3xl font-bold  '
-            >
-                You are already logged in
-            </h1>
-            <Link href={'/users/userdashboard'}>
-                <h1
-                    className='text-3xl font-bold  '
-                >
-                    Click here to go to dashboard
-                </h1>
-            </Link>
-        </PageUi>)
-
-    }
+  
     return (
         <PageUi
             className=' flex  flex-col items-center justify-center'
