d8388e5c47625718de62ebda5c1c5151dc5636bb
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-05-26 20:10:48 +0530

    login bug 1

--- musicwebnextjs/src/components/navbar/RightDropdownMenu.tsx
+++ musicwebnextjs/src/components/navbar/RightDropdownMenu.tsx
@@ -13,6 +13,7 @@ import userAvatarDBConfig from '@/config/dataBase/userPrefs/userAvatarDBConfig'
 import Link from 'next/link'
 import cryptoUtil from '@/lib/util/CryptoUtil'
 import LogOutBtn from '../Buttons/LogOutBtn'
+import ifNotUserAvatar from '@/config/dataBase/userPrefs/ifNotUserAvatar'
 
 
 function RightDropdownMenu() {
@@ -27,7 +28,7 @@ function RightDropdownMenu() {
                     <DropdownMenu>
                         <DropdownMenuTrigger className='cursor-pointer' asChild>
                             <img
-                                src={String(userAvatarDBConfig.getUserAvatarPreviewWithPrefs(userPrefs.avatar))}
+                                src={userPrefs.avatar ? String(userAvatarDBConfig.getUserAvatarPreviewWithPrefs(userPrefs?.avatar)): String(ifNotUserAvatar.getUserInitials())}
                                 alt="User"
                                 className='w-[40px] h-[40px] rounded-full'
                             />
